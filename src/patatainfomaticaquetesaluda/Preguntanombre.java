a14d79f069b84e88d5a667dcd662c6086b3e9898
jperezfontao <jperezfontao@victoria25.danielcastelao.org>
2017-11-09 09:26:28 +0100

    Commit saludo de nuestra gran Patata

---
+++ src/patatainfomaticaquetesaluda/Preguntanombre.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patatainfomaticaquetesaluda;
+
+
+
+/**
+ *
+ * @author ggarciaalvarez
+ */
+public class Preguntanombre {
+public static String nombre;
+
+    public Preguntanombre() {
+    }
+
+    public static String getNombre() {
+        return nombre;
+    }
+
+    public static void setNombre(String nombre) {
+        Preguntanombre.nombre = nombre;
+    }
+       
+
+   
+}
+
